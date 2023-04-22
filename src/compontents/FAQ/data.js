ba4a0a8c5eda12c905bf0fae53f428ea2bceb104
SudiptaDeyAnik08 <sudiptadeysd06@gmail.com>
2023-04-22 15:35:30 +0600

    data.js added

---
+++ src/compontents/FAQ/data.js
@@ -0,0 +1,26 @@
+export const faqdata =[
+    {
+        id:1,
+        title: "Lorem ipsum dolor sit amet consectetur a",
+        decription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores facere nisi eos, odio ex quod aliquam explicabo? Ex laudantium suscipit a placeat aspernatur officia nesciunt atque delectus! Minima, error quisquam."
+        
+    },
+    {
+        id:2,
+        title: "Lorem ipsum dolor sit amet consectetur a",
+        decription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores facere nisi eos, odio ex quod aliquam explicabo? Ex laudantium suscipit a placeat aspernatur officia nesciunt atque delectus! Minima, error quisquam."
+        
+    },
+    {
+        id:3,
+        title: "Lorem ipsum dolor sit amet consectetur a",
+        decription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores facere nisi eos, odio ex quod aliquam explicabo? Ex laudantium suscipit a placeat aspernatur officia nesciunt atque delectus! Minima, error quisquam."
+        
+    },
+    {
+        id:4,
+        title: "Lorem ipsum dolor sit amet consectetur a",
+        decription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores facere nisi eos, odio ex quod aliquam explicabo? Ex laudantium suscipit a placeat aspernatur officia nesciunt atque delectus! Minima, error quisquam."
+        
+    }
+]
